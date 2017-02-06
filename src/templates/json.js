2d12c22b6d55b19914619cb70ac91d589022a252
Patrick Vienneau <pvien027@gmail.com>
2017-02-05 19:34:58 -0500

    Revert export method in templates/json.js

--- src/templates/json.js
+++ src/templates/json.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     nonEmptyValue: (schema) => schema.array() || schema.object() || schema.literal(),
     value: (schema) => schema.nonEmptyValue() ||schema. empty(),
     object: (schema) => schema.parensLeft() && schema.objectValue() && schema.parensRight(),
